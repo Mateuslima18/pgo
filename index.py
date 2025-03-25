6c55a448334659100b7e4add2c8c13cbea1e2bac
Mateuslima18 <mateus.macedo@academico.ifpb.edu.br>
2025-03-25 19:45:57 +0000

    kkkkkkkkkkkk

--- index.py
+++ index.py
@@ -1,1 +1,13 @@
+class Smartphone:  
+    def __init__(self):  
+        self.marca = None
+        self.modelo =  None
+        self.armazenamento = None
+        self.cor =  None
+        self.preco = None 
 
+Smart1= Smartphone()
+
+Smartphone.nome ="melhor do ano"
+
+print(Smart1.nome)
